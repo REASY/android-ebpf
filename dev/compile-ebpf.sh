47f4c9fdbdb77dc3581d8692e211f1c64226d190
Artavazd Balaian <reasyu@gmail.com>
2025-10-27 18:26:14 +0800

    fix: symbolic link for aarch64-linux-gnu as well

--- dev/compile-ebpf.sh
+++ dev/compile-ebpf.sh
@@ -36,7 +36,7 @@ echo "Cloning and building libbpf"
 git clone --depth 1 https://github.com/libbpf/libbpf.git --branch v1.5.0 --single-branch && cd libbpf
 cd src && make -j$(nproc) && make install && make install_uapi_headers
 ln -s /usr/lib64/libbpf.so.1.5.0 /usr/lib/x86_64-linux-gnu/libbpf.so.1
-
+ln -s /usr/lib64/libbpf.so.1.5.0 /usr/lib/aarch64-linux-gnu/libbpf.so.1
 
 cd ~/code
 echo "Cloning and building bpftrace"
